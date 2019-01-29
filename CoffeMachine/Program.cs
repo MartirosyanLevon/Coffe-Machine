d7700fee1e56a90d02497142bcef42b05b16fe16
Lev# <mr.lmartirosyan888@gmail.com>
2019-01-30 01:55:12 +0400

    relise one

---
+++ CoffeMachine/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coffe cap siz: 1- Small 2- Middle 3- Big");
+            Console.WriteLine("Please make your choice");
+            string cofeSize = Console.ReadLine();
+            int cost = 0;
+
+            switch (cofeSize)
+            {
+                case "1":
+                    cost += 25;
+                    break;
+                case "2":
+                    cost += 25;
+                    goto case "1";
+                case "3":
+                    cost += 50;
+                    goto case "1";
+                default:
+                    Console.WriteLine("Wrong choice.Please choice 1,2 or 3");
+                    break;
+            }
+            if (cost != 0)
+            {
+                Console.WriteLine("Pay {0} cent \nThank you", cost);
+            }
+            else
+            {
+                Console.WriteLine("Please repeat selection!!!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
